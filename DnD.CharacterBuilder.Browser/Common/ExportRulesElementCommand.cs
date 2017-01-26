8d570cb72643385a289635493342ce3a143f2245
TDixon-Desktop <tdixon12@vols.utk.edu>
2017-01-26 08:36:32 -0500

    Updated specific rule extraction to use a switch and added roll functionality wich extracts ability modifier type

--- DnD.CharacterBuilder.Browser/Common/ExportRulesElementCommand.cs
+++ DnD.CharacterBuilder.Browser/Common/ExportRulesElementCommand.cs
@@ -27,11 +27,12 @@ namespace DnD.CharacterBuilder.Browser
             //Clipboard.SetText("Test");
             var rulesElement = parameter as RulesElement;
             List<string> rulesNeeded = new List<string>();
-            //[0]ActionType [1]AttackType [2] Keywords [3]Usage [4]Attack(what vs what) [5]Target [6]Hit [7]Effect [8]Miss
+            //[0]ActionType [1]AttackType [2] Keywords [3]Usage [4]Attack(what vs what) [5]Target [6]Hit [7]Effect [8]Miss [9]Modifier
 
-            var rvstring = "!power --format|dnd4e";
+            var rvstring = "!power {{\n --format|dnd4e\n";
+            var firstSpace = 0;
 
-            for (var i= 0; i < 9; i++)
+            for (var i= 0; i < 10; i++)
             {
                 rulesNeeded.Add(null);
             }
@@ -39,85 +40,70 @@ namespace DnD.CharacterBuilder.Browser
             //Go through the specific rules, saving any ones that we plan to use into a predetermined index in rulesNeeded
             foreach (SpecificRule element in rulesElement.SpecificRules)
             {
-                if (element.Name == "Action Type")
+                switch (element.Name)
                 {
-                    rulesNeeded[0] = element.Description;
-                    continue;
-                }
-
-                if (element.Name == "Attack Type")
-                {
-                    rulesNeeded[1] = element.Description;
-                    continue;
-                }
-
-                if (element.Name == "Keywords")
-                {
-                    rulesNeeded[2] = element.Description;
-                    continue;
-                }
-
-                if (element.Name == "Power Usage")
-                {
-                    rulesNeeded[3] = element.Description;
-                    continue;
-                }
-
-                if (element.Name == "Attack")
-                {
-                    rulesNeeded[4] = element.Description;
-                    continue;
-                }
-
-                if (element.Name == "Target")
-                {
-                    rulesNeeded[5] = element.Description;
-                    continue;
-                }
-
-                if (element.Name == "Hit")
-                {
-                    rulesNeeded[6] = element.Description;
-                    continue;
-                }
-
-                if (element.Name == "Effect")
-                {
-                    rulesNeeded[7] = element.Description;
-                    continue;
-                }
-
-                if(element.Name == "Miss")
-                {
-                    rulesNeeded[8] = element.Description;
-                    continue;
+                    case "Action Type":
+                        rulesNeeded[0] = element.Description;
+                        break;
+                    case "Attack Type":
+                        rulesNeeded[1] = element.Description;
+                        break;
+                    case "Keywords":
+                        rulesNeeded[2] = element.Description;
+                        break;
+                    case "Power Usage":
+                        rulesNeeded[3] = element.Description;
+                        break;
+                    case "Attack":
+                        rulesNeeded[4] = element.Description;
+                        //Extract the player's relevant modifier name from the attack specific rule string
+                        rulesNeeded[9] = element.Description;
+                        rulesNeeded[9] = rulesNeeded[9].Remove(0, 1);
+                        firstSpace = rulesNeeded[9].IndexOf(' ');
+                        rulesNeeded[9] = rulesNeeded[9].Remove(firstSpace, rulesNeeded[9].Length - firstSpace);
+                        break;
+                    case "Target":
+                        rulesNeeded[5] = element.Description;
+                        break;
+                    case "Hit":
+                        rulesNeeded[6] = element.Description;
+                        break;
+                    case "Effect":
+                        rulesNeeded[7] = element.Description;
+                        break;
+                    case "Miss":
+                        rulesNeeded[8] = element.Description;
+                        break;
                 }
             }
-
 
             //Format the export string itself, checking to make sure each field is present before concatenating it with the formatted string
             if (rulesElement.Name != null)
             {
                 rvstring += " --name|";
                 rvstring += rulesElement.Name;
+                rvstring += "\n";
             }
 
             if (rulesNeeded[0] != null)
             {
                 rvstring += " --leftsub|";
                 rvstring += rulesNeeded[0];
+                rvstring += "\n";
             }
 
             if (rulesNeeded[1] != null)
             {
                 rvstring += " --rightsub|";
                 rvstring += rulesNeeded[1];
+                rvstring += "\n";
             }
 
             if (rulesNeeded[2] != null)
             {
                 rvstring += " --Keywords|";
                 rvstring += rulesNeeded[2];
+                rvstring += "\n";
             }
 
             //if (rulesElement.Source != null)
@@ -130,50 +116,62 @@ namespace DnD.CharacterBuilder.Browser
             {
                 rvstring += " --Usage|";
                 rvstring += rulesNeeded[3];
+                rvstring += "\n";
             }
 
             if (rulesNeeded[5] != null)
             {
                 rvstring += " --Target|";
                 rvstring += rulesNeeded[5];
+                rvstring += "\n";
             }
 
             if (rulesElement.FlavorText != null)
             {
                 rvstring += " --Flavor|";
                 rvstring += rulesElement.FlavorText;
+                rvstring += "\n";
             }
 
             //if (rulesElement.Prerequisites != null)
             //{
             //    rvstring += " --Prereq|";
             //    rvstring += rulesElement.Prerequisites;
+            //    rvstring += "\n";
             //}
 
             if (rulesNeeded[4] != null)
             {
                 rvstring += " --Attack|";
                 rvstring += rulesNeeded[4];
+                rvstring += "\n";
+                rvstring += "--Roll|[[1d20+@{";
+                rvstring += rulesNeeded[9];
+                rvstring += "-Mod}]]\n";
             }
 
             if (rulesNeeded[6] != null)
             {
                 rvstring += " --Hit|";
                 rvstring += rulesNeeded[6];
+                rvstring += "\n";
             }
 
             if (rulesNeeded[7] != null)
             {
                 rvstring += " --Effect|";
                 rvstring += rulesNeeded[7];
+                rvstring += "\n";
             }
 
             if (rulesNeeded[8] != null)
             {
                 rvstring += " --Miss|";
                 rvstring += rulesNeeded[8];
+                rvstring += "\n";
             }
 
+            rvstring += "}}";
             Clipboard.SetText(rvstring);
         }
     }
